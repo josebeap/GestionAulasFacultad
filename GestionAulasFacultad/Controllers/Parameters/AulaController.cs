59a1ce9cf649dca38bd950518054353b3fcb7a94
Juan David Osorio <juan.1701714291@ucaldas.edu.co>
2022-11-27 08:24:01 -0500

    Creación de capa de datos. Se desacopla esta capa de la de vista-logica, por lo que ahora los controladores y las vistas acceden a esta capa

--- GestionAulasFacultad/Controllers/Parameters/AulaController.cs
+++ GestionAulasFacultad/Controllers/Parameters/AulaController.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using GestionAulasFacultad.ModeloBD;
+using AccesoDeDatos.ModeloDeDatos;
 
 namespace GestionAulasFacultad.Controllers.Parameters
 {
---
+++ ImplPersonaDatos.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using AccesoDeDatos.ModeloDeDatos;
+
+namespace AccesoDeDatos.Implementacion
+{ 
+	public class ImplPersonaDatos
+	{
+		public IEnumerable<tb_persona> ListarRegistros()
+        {
+			var lista = new List<tb_persona>();
+			using(SoftwareBDEntities bd = new SoftwareBDEntities())
+            {
+				lista = bd.tb_persona.ToList();
+            }
+			return lista;
+        }
+
+        /// <summary>
+        /// Método para almacenar un registro
+        /// </summary>
+        /// <param name="registro">el registro a almacenar</param>
+        /// <returns>true cuando se almacena y false cuando ya existe un registro igual o una excepción</returns>
+        public bool GuardarRegistro(tb_persona registro)
+        {
+            try
+            {
+                using (SoftwareBDEntities bd = new SoftwareBDEntities())
+                {
+                    // verificación de la existencia de un registro con el mismo nombre
+                    if (bd.tb_persona.Where(x => x.nombre.ToLower().Equals(registro.Nombre.ToLower())).Count() > 0)
+                    {
+                        return false;
+                    }
+                   
+                    
+                    bd.tb_persona.Add(reg);
+                    bd.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// Método de búsqueda de un registro
+        /// </summary>
+        /// <param name="id">id del registro a buscar</param>
+        /// <returns>el objeto con el id buscado o null cuando no exista</returns>
+        public tb_persona BuscarRegistro(int id)
+        {
+            using (SoftwareBDEntities bd = new SoftwareBDEntities())
+            {
+                tb_persona registro = bd.tb_persona.Find(id);
+                if (registro != null)
+                {
+                    return registro;
+                }
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Método para editar un registro
+        /// </summary>
+        /// <param name="registro">el registro a editar</param>
+        /// <returns>true cuando se edita y false cuando no existe el registro o una excepción</returns>
+        public bool EditarRegistro(tb_persona registro)
+        {
+            try
+            {
+                using (SoftwareBDEntities bd = new SoftwareBDEntities())
+                {
+                    // verificación de la existencia de un registro con el mismo id
+                    if (bd.tb_persona.Where(x => x.id == registro.Id).Count() == 0)
+                    {
+                        return false;
+                    }
+                    
+                    bd.Entry(registro).State = EntityState.Modified;
+                    bd.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// Método de eliminar un registro por el id
+        /// </summary>
+        /// <param name="id">id del registro a eliminar</param>
+        /// <returns>true cuando se elimina, false cuando no existe el registro o una excepción</returns>
+        public bool EliminarRegistro(int id)
+        {
+            try
+            {
+                using (SoftwareBDEntities bd = new SoftwareBDEntities())
+                {
+                    // verificación de la existencia de un registro con el mismo id
+                    tb_persona registro = bd.tb_persona.Find(id);
+                    if (registro == null || registro.tb_vehiculo.Count() > 0)
+                    {
+                        return false;
+                    }
+                    bd.tb_persona.Remove(registro);
+                    bd.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+    }
+}
