46c7706cda5b9932d60cce9fded6b50743f61460
josebeap <josebeap99@gmail.com>
2022-12-20 15:50:43 -0500

    Implementacion de SSO User y Roles

---
+++ AccesoDeDatos/DbModel/SecurityModule/RoleDbModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDeDatos.DbModel.SecurityModule
+{
+    public class RoleDbModel
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+
+
+        private bool removed;
+
+        public bool Removed
+        {
+            get { return removed; }
+            set { removed = value; }
+        }
+    }
+}
